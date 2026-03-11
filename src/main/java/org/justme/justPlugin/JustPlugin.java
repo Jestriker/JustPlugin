b59ce3c142032f82c73f81726cd224128062bb2c
Jestriker <liambenrey@gmail.com>
2026-03-11 21:11:48 +0200

    initial commit: add JustPlugin with Gradle setup and .gitignore

---
+++ src/main/java/org/justme/justPlugin/JustPlugin.java
@@ -0,0 +1,17 @@
+package org.justme.justPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JustPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
